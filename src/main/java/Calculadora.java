9e69cd1fa8c7f1541d4b46ffe1ad1007742f0cde
dionilsonmaia <142270986+dionilsonmaia@users.noreply.github.com>
2024-03-21 00:29:30 +0000

    Atualização da calculadora

--- src/main/java/Calculadora.java
+++ src/main/java/Calculadora.java
@@ -1,30 +1,96 @@
+import java.util.Scanner;
+
 class Calculadora{
-  int num1;
-  int num2;
-  int total;
+  private int num1;
+  private int num2;
+  private int total;
 
-  public void soma(int n1, int n2){
-    System.out.println("Bem vindo a calculadora");
-    System.out.println("Oporeção de soma");
-    this.num1 = n1;
-    this.num2 = n2;
-    total = num1 + num2;
-    System.out.println("O total da soma é: " + total);
-   }
-  public void subtracao(int n1, int n2){
-  System.out.println("Oporeção de subtração");
-  this.num1 = n1;
-  this.num2 = n2;
-  total = num1 - num2;
-  System.out.println("O total da subtração é: " + total);
-  }
-  public void multiplicacao(int n1, int n2){
-  System.out.println("Oporeção de multiplicação");
-  this.num1 = n1;
-  this.num2 = n2;
-  total = num1 * num2;
-  System.out.println("O total da multiplicação é: " + total);
+Scanner ler = new Scanner(System.in);
+//matodo menu
+  
+  //método para somar
+  public void soma(){
+    Scanner n1 = new Scanner(System.in);
+    Calculadora calc = new Calculadora();
 
+    int total;
+
+    System.out.println("Calculadora de soma\n\nInsirao 1º nº: ");
+    calc.setNum1(n1.nextInt());
+    System.out.println("Insira o 2º nº: ");
+    calc.setNum2(n1.nextInt());
+    total = calc.getNum1() + calc.getNum2();
+    System.out.println("A soma dos valores é: " + total);
+    
   }
+
+  //método para subtração
+  public void subtracao(){
+    Scanner n1 = new Scanner(System.in);
+    Calculadora calc = new Calculadora();
+
+    int total;
+
+    System.out.println("Calculadora de subtração\n\nInsirao 1º nº: ");
+    calc.setNum1(n1.nextInt());
+    System.out.println("Insira o 2º nº: ");
+    calc.setNum2(n1.nextInt());
+    total = calc.getNum1() - calc.getNum2();
+    System.out.println("A subtração dos valores é: " + total);
+    
+  }
+
+  //método para multiplicação
+  public void multiplicacao(){
+    Scanner n1 = new Scanner(System.in);
+    Calculadora calc = new Calculadora();
+
+    int total;
+
+    System.out.println("Calculadora de multiplicação\n\nInsirao 1º nº: ");
+    calc.setNum1(n1.nextInt());
+    System.out.println("Insira o 2º nº: ");
+    calc.setNum2(n1.nextInt());
+    total = calc.getNum1() * calc.getNum2();
+    System.out.println("A multiplicação dos valores é: " + total);
+    
+  }
+  //método para divisão
+  public void divisao(){
+    Scanner n1 = new Scanner(System.in);
+    Calculadora calc = new Calculadora();
+
+    int total;
+
+    System.out.println("Calculadora de divisão\n\nInsirao 1º nº: ");
+    calc.setNum1(n1.nextInt());
+    System.out.println("Insira o 2º nº: ");
+    calc.setNum2(n1.nextInt());
+    total = calc.getNum1() / calc.getNum2();
+    System.out.println("A divisão dos valores é: " + total);
+    
+  }
+public int getNum1() {
+	return num1;
 }
+public void setNum1(int num1) {
+	this.num1 = num1;
+}
+public int getNum2() {
+	return num2;
+}
+public void setNum2(int num2) {
+	this.num2 = num2;
+}
+public int getTotal() {
+	return total;
+}
+public void setTotal(int total) {
+	this.total = total;
+}
+  
+ 
+
+}
+ 
   
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,22 +7,17 @@ public class Main {
   
 
 
-  
+  //metodo para somar
   public static void main(String[] args) {
   Calculadora calc = new Calculadora();
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Digite o primeiro número: ");
-    int n1 = sc.nextInt();
-    System.out.println("Digite o segundo número: ");
-    int n2 = sc.nextInt();
-    calc.subtracao(n1, n2);
-    
-    
-    
-    
+  calc.soma();  
+    calc.subtracao();
+    calc.multiplicacao();
+    calc.divisao();
   }
-
+  
   // @Test
+  
   // void addition() {
   //     assertEquals(2, 1 + 1);
   // }
